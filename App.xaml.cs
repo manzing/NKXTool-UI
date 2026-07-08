2ee6d87137d91e06b81311a0b5e692075407f774
manzing <manzing@gmail.com>
2026-07-08 18:34:37 +0200

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Windows;
 
 namespace NkxToolUI;
 
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
     protected override void OnStartup(StartupEventArgs e)
     {
