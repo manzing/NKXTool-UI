8b73fa156e152fa19bc1e9271b7b1b84d672c3a6
manzing <manzing@gmail.com>
2026-07-09 16:23:08 +0200

    Refactor methods to use expression-bodied syntax
    Refactored methods to use expression-bodied members for brevity.

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -14,7 +14,6 @@ public partial class MainWindow : Window
         InitializeComponent();
         ApplyTranslations();
         InitializeThemeSelector();
-
         ThemeManager.Current.ThemeChanged += ThemeManager_OnThemeChanged;
         Closed += MainWindow_Closed;
     }
@@ -29,83 +28,35 @@ public partial class MainWindow : Window
         BrowseButton.Content = _localization.Get("action.browse");
     }
 
-    private void InitializeThemeSelector()
-    {
-        SetThemeComboSelection(ThemeManager.Current.CurrentTheme);
-    }
+    private void InitializeThemeSelector() => SetThemeComboSelection(ThemeManager.Current.CurrentTheme);
 
-    private void ThemeManager_OnThemeChanged(object? sender, EventArgs e)
-    {
-        SetThemeComboSelection(ThemeManager.Current.CurrentTheme);
-    }
+    private void ThemeManager_OnThemeChanged(object? sender, EventArgs e) => SetThemeComboSelection(ThemeManager.Current.CurrentTheme);
 
     private void SetThemeComboSelection(string themeName)
     {
         _isUpdatingThemeSelector = true;
-
         try
         {
             foreach (var item in ThemeComboBox.Items.OfType<ComboBoxItem>())
-            {
                 if (string.Equals(item.Content?.ToString(), themeName, StringComparison.OrdinalIgnoreCase))
                 {
                     ThemeComboBox.SelectedItem = item;
                     break;
                 }
-            }
         }
-        finally
-        {
-            _isUpdatingThemeSelector = false;
-        }
+        finally { _isUpdatingThemeSelector = false; }
     }
 
     private void ThemeComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
     {
-        if (_isUpdatingThemeSelector)
-        {
-            return;
-        }
-
-        if (ThemeComboBox.SelectedItem is ComboBoxItem item &&
-            item.Content is string themeName)
-        {
+        if (_isUpdatingThemeSelector) return;
+        if (ThemeComboBox.SelectedItem is ComboBoxItem item && item.Content is string themeName)
             ThemeManager.Current.ApplyTheme(themeName);
-        }
     }
 
-    private void MainWindow_Closed(object? sender, EventArgs e)
-    {
-        ThemeManager.Current.ThemeChanged -= ThemeManager_OnThemeChanged;
-    }
+    private void MainWindow_Closed(object? sender, EventArgs e) => ThemeManager.Current.ThemeChanged -= ThemeManager_OnThemeChanged;
 
-    private void PackButton_OnClick(object sender, RoutedEventArgs e)
-    {
-        var window = new PackWindow
-        {
-            Owner = this
-        };
-
-        window.ShowDialog();
-    }
-
-    private void UnpackButton_OnClick(object sender, RoutedEventArgs e)
-    {
-        var window = new UnpackWindow
-        {
-            Owner = this
-        };
-
-        window.ShowDialog();
-    }
-
-    private void BrowseButton_OnClick(object sender, RoutedEventArgs e)
-    {
-        var window = new BrowseWindow
-        {
-            Owner = this
-        };
-
-        window.ShowDialog();
-    }
+    private void PackButton_OnClick(object sender, RoutedEventArgs e) { new PackWindow { Owner = this }.ShowDialog(); }
+    private void UnpackButton_OnClick(object sender, RoutedEventArgs e) { new UnpackWindow { Owner = this }.ShowDialog(); }
+    private void BrowseButton_OnClick(object sender, RoutedEventArgs e) { new BrowseWindow { Owner = this }.ShowDialog(); }
 }
